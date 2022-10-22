6eaf13715748187e8c6c7c552532a8bd359cff66
BroderViktor <viktor.asheim@gmail.com>
2022-10-22 17:24:20 +0200

    test push to web

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+/*app.use(cors({
+    origin: '*',
+    methods: 'get'
+}));
+*/
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+app.get("/api", (req, res) => {
+    res.json({message : "effe"});
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log('Server listening on ', PORT);
+})
