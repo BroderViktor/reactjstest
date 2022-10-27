d51471ca3a643ec80fc571a9bee7f9c877e6407a
BroderViktor <viktor.asheim@gmail.com>
2022-10-27 23:09:22 +0200

    fefef

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,34 @@ function App() {
 
   const [data, setData] = React.useState(null);
 
+  /*React.useEffect(() => {
+    fetch("https://tt02.altinn.no/api/authentication/authenticatewithpassword", {
+      method: "POST",
+      headers: ({
+        //'Authorization': '7A2BA1FE-384E-4793-BD77-58561B15D766', 
+        'Host': 'tt02.altinn.no',
+        'Content-Type': 'application/hal+json',
+        'Accept': 'application/hal+json',
+        'ApiKey': '7A2BA1FE-384E-4793-BD77-58561B15D766',
+      }),
+      body: ({
+        "UserName": "brasa1",
+        "UserPassword": "passord123"
+      })
+    }).then((res) => console.log(res)).then((data) => console.log(data))
+  });*/
   React.useEffect(() => {
-    fetch("/api")
-      //.then((res) => console.log(res))
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+    fetch("https://tt02.altinn.no/api/metadata?language=1044", {
+      method: "GET",
+      headers: ({
+        //'Authorization': '7A2BA1FE-384E-4793-BD77-58561B15D766', 
+        'Host': 'tt02.altinn.no',
+        'Content-Type': 'application/hal+json',
+        'Accept': 'application/hal+json',
+        //'ApiKey': '7A2BA1FE-384E-4793-BD77-58561B15D766',
+      }),
+    }).then((res) => console.log(res.json()))
+  });
 
   return (
     <div className="App">
